44292d0ff26f5deb9b925eaf1d9f4d34a048e9db
mlamont8 <debiandmiat@hotmail.com>
2016-05-11 22:25:49 -0400

    reads clicks

--- script.js
+++ script.js
@@ -50,13 +50,47 @@ $('.prebutton').click(function () {
     comp = gamePlay(user);
     console.log('user', user);
     console.log('comp', comp);
+    //empty board spaces
+    var board = {b1:null, b2:null, b3:null, b4:null, b5:null, b6:null, b7:null, b8:null, b9:null};
+    // begin play
+    tictacgo(board, user, comp);
 });
 
    	// Done with Popup Page
-var board = {b1:null, b2:null, b3:null, b4:null, b5:null, b6:null, b7:null, b8:null, b9:null};
+
 
 
 });
+
+//gameplay function
+function tictacgo(board, user, comp){
+    console.log(user, comp);
+    console.log(board);
+    $('#b1').html(comp);
+    board.b1 = comp;
+    board = humanMove(board, user, comp);
+  
+}
+
+function humanMove(board, user, comp){
+     var attempt; $(".blk").click(function() {
+        attempt = $(this).attr('id');
+         boardUpdate(attempt, board, user,comp);
+    });
+    console.log(attempt);
+}
+function boardUpdate(attempt, board, user,comp){
+    console.log(board);
+    //check to see if already clicked
+    if (attempt !== null){
+        alert("Try Another Move");
+    }
+    //update the board object with attempt key value
+    console.log(attempt, user);
+        board[attempt] = user;
+    console.log(board);
+    
+}
     function gamePlay(user){
         if (user === 'X'){
             return 'O';
